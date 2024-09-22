b8aeedd3b5d7c3f1ca432f9274ff5b180367b208
Ihor Haiduk <spartanfastfun@gmail.com>
2024-09-22 18:36:31 +0300

    Created models for Company and Ticker

---
+++ StockExchangeWebsite/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockExchangeWebsite.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TickerId { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public int NumWorkers { get; set; }
+        public string ContactPhone {  get; set; }
+        public DateOnly FoundationDate { get; set; }
+        public string RegistrationAddress {  get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ StockExchangeWebsite/Models/Ticker.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockExchangeWebsite.Models
+{
+    public class Ticker
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public string Symbol { get; set; }
+        public double CurrentValue { get; set; }
+        public double OpenValue { get; set; }
+        public double CloseValue { get; set; }
+        public double TradingVolume { get; set; }
+    }
+}
